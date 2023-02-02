28b72c325fe21769a3698897feddbdb67e76c55a
Nicolas Peredo <nicolas.peredo@globant.com>
2023-02-02 17:26:02 -0300

    Webpack alias first iteration

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,28 @@
+const path = require("path");
+
 module.exports = {
     mode: "development",
     watch: true,
     entry: "./src/index.tsx",
     output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist"
+        filename: "bundle.js",
+
+        path: path.resolve(__dirname, "/dist"),
     },
     resolve: {
-      extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: [".ts", ".tsx", ".js", ".json"],
+        alias: {
+            Components: path.resolve(__dirname, "src/components"),
+            "@UI": path.resolve(__dirname, "src/UI/*"),
+        },
     },
     devtool: "source-map",
     module: {
-      rules: [
-        { test: /\.scss$/, use: [ "style-loader", "css-loader", "sass-loader" ] }
-      ]
-    }
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
 };
