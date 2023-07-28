f13a400784cf674dae7e8717f76ea29dea25ebc0
Nicolas Peredo <nicolas.peredo@globant.com>
2023-07-28 17:15:09 -0400

    Adding fake timers to test setTimeout functionality

--- src/components/Main/Main.test.js
+++ src/components/Main/Main.test.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render, fireEvent, cleanup, screen } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  screen,
+  act,
+} from "@testing-library/react";
 import Main from "./Main";
 import { Provider } from "react-redux";
 import store from "../../store/store";
@@ -28,18 +34,26 @@ describe("Testing the functionality on Main", () => {
       </Provider>
     );
     expect(screen.getByRole("main")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: POMODORO })).toBeInTheDocument();
     expect(
-      screen.getByRole("button", { name: "Pomodoro" })
+      screen.getByRole("button", { name: SHORT_BREAK })
     ).toBeInTheDocument();
     expect(
-      screen.getByRole("button", { name: "Short Break" })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", { name: "Long Break" })
+      screen.getByRole("button", { name: LONG_BREAK })
     ).toBeInTheDocument();
   });
 });
+
 describe("Triggering different button events", () => {
+  beforeEach(() => {
+    jest.useFakeTimers(); // set up fake timers
+    jest.spyOn(global, "setInterval");
+  });
+
+  afterEach(() => {
+    jest.useRealTimers(); // restore real timers
+    cleanup(); // clean up any mounted components
+  });
   test("Pomodoro active button", () => {
     render(
       <Provider store={store}>
@@ -51,6 +65,23 @@ describe("Triggering different button events", () => {
     });
     expect(pomodoroButton).toHaveClass(`btn--active`);
     expect(screen.getByText(TIMER_CONFIG[POMODORO])).toBeInTheDocument();
+    fireEvent.click(pomodoroButton);
+    const startButton = screen.getByRole("button", {
+      name: "START",
+    });
+
+    //Moking the play button as this is not implemented in testing environments
+    HTMLMediaElement.prototype.play = jest.fn();
+    fireEvent.click(startButton);
+    //Check if the sound reproduces.
+    expect(HTMLMediaElement.prototype.play).toHaveBeenCalled();
+    expect(setInterval).toHaveBeenCalledTimes(1);
+
+    //Fastforward in time to check the timer after 1 minute .
+    act(() => {
+      jest.advanceTimersByTime(1 * 60 * 1000);
+    });
+    expect(screen.getByText("44:00")).toBeInTheDocument();
   });
   const mockHandler = jest.fn();
   test("Testing Short Break button", () => {
@@ -63,8 +94,25 @@ describe("Triggering different button events", () => {
       name: SHORT_BREAK,
     });
     fireEvent.click(shortBreakButton);
-
     expect(screen.getByText(TIMER_CONFIG[SHORT_BREAK])).toBeInTheDocument();
+
+    const startButton = screen.getByRole("button", {
+      name: "START",
+    });
+
+    //Moking the play button as this is not implemented in testing environments
+    HTMLMediaElement.prototype.play = jest.fn();
+    fireEvent.click(startButton);
+    //Check if the sound reproduces.
+    expect(HTMLMediaElement.prototype.play).toHaveBeenCalled();
+    expect(setInterval).toHaveBeenCalledTimes(1);
+
+    //Fastforward in time to check the timer after 1 minute .
+    act(() => {
+      jest.advanceTimersByTime(1 * 60 * 1000);
+    });
+    expect(screen.getByText("4:00")).toBeInTheDocument();
+
     //expect(shortBreakButton).toHaveClass(`btn--active`);
   });
 
@@ -78,10 +126,24 @@ describe("Triggering different button events", () => {
       name: LONG_BREAK,
     });
     fireEvent.click(longBreakButton);
-
     expect(screen.getByText(TIMER_CONFIG[LONG_BREAK])).toBeInTheDocument();
+    const startButton = screen.getByRole("button", {
+      name: "START",
+    });
+
+    //Moking the play button as this is not implemented in testing environments
+    HTMLMediaElement.prototype.play = jest.fn();
+    fireEvent.click(startButton);
+    //Check if the sound reproduces.
+    expect(HTMLMediaElement.prototype.play).toHaveBeenCalled();
+    expect(setInterval).toHaveBeenCalledTimes(1);
+
+    //Fastforward in time to check the timer after 1 minute .
+    act(() => {
+      jest.advanceTimersByTime(1 * 60 * 1000);
+    });
+    expect(screen.getByText("14:00")).toBeInTheDocument();
+
     //expect(longBreakButton).toHaveClass(`btn--active`);
   });
 });
-/*   describe("Testing timer", () => {});
-  describe("Testing task list", () => {}); */
