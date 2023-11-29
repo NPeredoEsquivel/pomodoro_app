e69bc9f2f1be1758e29a056922925a42af4505f7
Nicolas Peredo <nicolas.peredo@globant.com>
2023-11-29 15:49:27 -0300

    final changes to auth

--- backend/routes/auth/auth.mjs
+++ backend/routes/auth/auth.mjs
@@ -1,20 +1,37 @@
 import express from 'express';
 import db from '../../db/conn.mjs';
-import * as bcrypt from 'bcrypt'
-import { isValidEmail, isValidPassword, isValidText } from '../../validation/validation.mjs'
+import { createJSONToken, isValidPassword, hashPassword } from '../../util/auth.mjs';
+import { isValidEmail, isValidText } from '../../validation/validation.mjs'
 
 const router = express.Router();
 
 router.post("/signin", async (req, res) => {
   let collection = db.collection('users');
-  let result = await collection.findOne({ username: req.body.username });
+  const data = req.body;
+
+  let errors = {};
+  if (!isValidText(data.password)) {
+    errors.password = 'Invalid password. Must be at least 6 characters long.';
+  }
   
+  if (!isValidEmail(data.email)) {
+    errors.email = 'Invalid email format.';
+  } 
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(422).json({
+      message: 'User signin failed due to validation errors.',
+      errors,
+    });
+  }
+
+  let result = await collection.findOne({ email: data.email });
   if (!result) {
     return res.status(404).send({ message: "User doesn't exist in database"});
   }
 
-  const passwordIsValid = bcrypt.compareSync(
-    req.body.password,
+  const passwordIsValid = isValidPassword(
+    data.password,
     result.scryptPass
   );
 
@@ -22,23 +39,33 @@ router.post("/signin", async (req, res) => {
     return res.status(401).send({ message: "Invalid Password!" });
   }
 
-  res.status(200).send({
-    id: result._id,
-    username: result.username,
-    password: result.password,
-  });
+
+  const token = createJSONToken(data.email);
+  res.json({ token });
 })
 
 router.post("/signup", async (req, res, next) => {
   const data = req.body;
 
   let errors = {};
+  if (!isValidText(data.password, 6)) {
+    errors.password = 'Invalid password. Must be at least 6 characters long.';
+  }
 
-  //Validations here.
+  if (!isValidEmail(data.email)) {
+    errors.email = 'Invalid email format.';
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(422).json({
+      message: 'User signin failed due to validation errors.',
+      errors,
+    });
+  }
 
   try {
     const collection = db.collection('users');
-    let userInDB = await collection.findOne({ username: data.username });
+    let userInDB = await collection.findOne({ email: data.email });
     if (userInDB && Object.values(userInDB).length > 0) {
       return res.status(422).json({
         message: "Register failed, user alrady in database.",
@@ -46,11 +73,13 @@ router.post("/signup", async (req, res, next) => {
       });
     }
 
-    const {username, password} = data
-    const scryptPass = bcrypt.hashSync(password, 8)
+    const {email, password} = data;
+    const scryptPass = hashPassword(password);
 
-    const result = await collection.insertOne({username, scryptPass});
-    res.status(201).json({ message: "User saved.", event: result });
+    const result = await collection.insertOne({email, scryptPass});
+
+    const authToken = createJSONToken(email);
+    res.status(201).json({ message: "User saved.", event: result, token: authToken });
   } catch (error) {
     next(error);
   }
