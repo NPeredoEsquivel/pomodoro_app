c5b4c6b64c40b71f8ddd03d95c50aa8269d59b26
Nicolas Peredo <nicolas.peredo@globant.com>
2023-12-18 15:52:03 -0300

    Adding observations

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import Root from "./Root/Root";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+
+export {Root, Home, Login, Register}
