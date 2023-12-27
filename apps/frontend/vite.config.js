db9ab22360bc64b79631e30b554e0f8c0be93de5
Nicolas Peredo <nperedoesquivel@gmail.com>
2023-12-27 11:26:09 -0300

    Adding alias path to vite and ts

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import image from "@rollup/plugin-image";
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@UI': path.resolve(__dirname, './src/UI'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+    },
+  },
   plugins: [image(), react({ include: "**/*.{jsx,tsx}" })],
   server: {
     proxy: {
