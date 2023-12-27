0c423008cc0f8b0715c9703d7e68719f976e3db8
Nicolas Peredo <nperedoesquivel@gmail.com>
2023-12-27 13:15:35 -0300

    Adding env configurations

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import image from "@rollup/plugin-image";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [image(), react({ include: "**/*.{jsx,tsx}" })],
-  server: {
-    proxy: {
-      '/backend': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    /* define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    }, */
+    plugins: [image(), react({ include: "**/*.{jsx,tsx}" })],
+    server: {
+      proxy: {
+        '/backend': {
+          target: env.VITE_DEV_URL,
+          changeOrigin: true,
+        }
       }
     }
   }
