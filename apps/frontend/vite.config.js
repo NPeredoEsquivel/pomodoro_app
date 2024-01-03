303957cf1edabc5fefa68175b11a16515e48d7e9
Nicolas Peredo <nperedoesquivel@gmail.com>
2024-01-03 15:46:01 -0300

    Forgot to merge the current vite file

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -3,26 +3,33 @@ import react from "@vitejs/plugin-react";
 import image from "@rollup/plugin-image";
 import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@store': path.resolve(__dirname, './src/store'),
-      '@UI': path.resolve(__dirname, './src/UI'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@lib': path.resolve(__dirname, './src/lib'),
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  },
-  plugins: [image(), react({ include: "**/*.{jsx,tsx}" })],
-  server: {
-    proxy: {
-      '/backend': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src/'),
+        '@assets': path.resolve(__dirname, './src/assets/'),
+        '@components': path.resolve(__dirname, './src/components/'),
+        '@pages': path.resolve(__dirname, './src/pages/'),
+        '@store': path.resolve(__dirname, './src/store/'),
+        '@UI': path.resolve(__dirname, './src/UI/'),
+        '@utils': path.resolve(__dirname, './src/utils/'),
+        '@lib': path.resolve(__dirname, './src/lib/'),
+      },
+    },
+    plugins: [image(), react({ include: "**/*.{jsx,tsx}" })],
+    server: {
+      proxy: {
+        '/backend': {
+          target: env.VITE_DEV_URL,
+          changeOrigin: true,
+        }
       }
     }
   }
